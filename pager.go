b9b23e3033c1f9271471b5656935687762d88a75
IsmailT0 <ismailtemuroglu2819@gmail.com>
2026-01-09 21:33:11 +0300

    feat: add Pager struct for file-based paging with read, write, sync, and close methods

---
+++ pager.go
@@ -0,0 +1,57 @@
+package gokv
+
+import (
+	"fmt"
+	"os"
+)
+
+const PageSize = 4096
+
+type Pager struct {
+	file *os.File
+}
+
+func NewPager(filename string) (*Pager, error) {
+	// 0600 = Read/Write for user, No permissions for group/others
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Pager{
+		file: file,
+	}, nil
+}
+
+func (p *Pager) Read(pageID int) ([]byte, error) {
+	offset := int64(pageID * PageSize)
+
+	buff := make([]byte, PageSize)
+
+	// ReadAt returns the number of bytes read (n) and an error.
+	// If n < PageSize, we might have hit EOF (end of file).
+	_, err := p.file.ReadAt(buff, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return buff, nil
+}
+
+func (p *Pager) Write(pageID int, data []byte) error {
+	if len(data) > PageSize {
+		return fmt.Errorf("data too large for page")
+	}
+
+	offset := int64(pageID * PageSize)
+	_, err := p.file.WriteAt(data, offset)
+	return err
+}
+
+func (p *Pager) Sync() error {
+	return p.file.Sync()
+}
+
+func (p *Pager) Close() error {
+	return p.file.Close()
+}
